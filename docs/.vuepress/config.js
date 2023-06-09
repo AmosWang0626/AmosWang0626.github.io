aab4252c19c48e8947a7da58e9a962b5e39ae68b
amos.wang <1833063210@qq.com>
2023-06-09 21:21:53 +0800

    copy from amos_blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,120 @@
+const { defaultTheme } = require('vuepress')
+const { searchPlugin } = require('@vuepress/plugin-search')
+
+module.exports = {
+  lang: 'zh-CN',
+  title: "JAVA技术学习笔记",
+  description: 'https://www.eyeo.cn/',
+  head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
+  // custom config
+  dest: './dist',
+  public: './asset/public',
+  port: 8088,
+  open: true,
+  // theme config
+  theme: defaultTheme({
+    // Logo 配置
+    logo: '/images/logo.png',
+    logoDark: '/images/logo-dark.png',
+    // 其他配置
+    editLink: false,
+    repo: 'https://github.com/AmosWang0626/amos_blog',
+    // 导航栏 https://v2.vuepress.vuejs.org/zh/reference/default-theme/config.html#navbar
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        // java/spring/mybatis/
+        text: 'Java技术栈',
+        link: '/backend/'
+      },
+      {
+        // linux/docker/nginx/
+        text: 'Ops相关',
+        link: '/ops/'
+      },
+      {
+        text: '杂货铺',
+        link: '/boutique/',
+      }
+    ],
+    // 侧边栏, 可为每个URL指定侧边栏 https://v2.vuepress.vuejs.org/zh/reference/default-theme/config.html#sidebar
+    sidebar: {
+      '/backend/': [
+        {
+          text: 'Java基础',
+          children: [
+            {
+              text: 'String.matches() 性能测试',
+              link: '/backend/java/base/1-String.matches()性能测试.md'
+            },
+            {
+              text: '万能的HelloWorld',
+              link: '/backend/java/base/2-万能的HelloWorld.md'
+            },
+          ]
+        },
+        {
+          text: 'Java并发编程',
+          children: [
+            {
+              text: 'Java高效并发',
+              link: '/backend/java/concurrent/1-Java高效并发.md'
+            }
+          ]
+        },
+        {
+          text: '设计模式',
+          children: [
+            {
+              text: '设计模式概述',
+              link: '/backend/design_mode/1-设计模式概述.md'
+            }
+          ]
+        },
+        {
+          text: 'MySQL',
+          children: [
+            {
+              text: 'MySQL概览',
+              link: '/backend/mysql/1-MySQL概览.md'
+            }
+          ]
+        },
+      ],
+      '/ops/': [
+        {
+          text: 'Ops',
+          link: '/ops/'
+        }
+      ],
+      '/boutique/': [
+        {
+          text: '前端技术',
+          children: [
+            {
+              text: '编写可维护的JavaScript',
+              link: '/boutique/front/01.编写可维护的JavaScript.md'
+            },
+            {
+              text: 'Hexo挂掉自动重启',
+              link: '/boutique/front/02.Hexo挂掉自动重启.md'
+            }
+          ]
+        }
+      ],
+    },
+    notFound: [
+      "你访问的页面飞走了～",
+      "页面飞走了～",
+      "您访问的页面飞走了～"
+    ],
+    backToHome: "回到首页",
+  }),
+  plugins: [
+    searchPlugin({
+    })
+  ]
+}
---
+++ vuepress.md
@@ -0,0 +1,40 @@
+# Amos' Blog
+
+## 很开心，又发现一个神器 vuepress
+
+由于比较喜欢原生的东西，就从0开始折腾吧。
+
+对比下 Hexo 吧，自我感觉 Hexo 的分类做得不太显眼。
+
+就像思维导图更能看清大图一样，有个清晰的分类大多时候是事半功倍的，毕竟从第一篇一直往后翻也是不现实的哈。
+
+## Quick Start
+
+```
+# install
+yarn install
+
+# start
+yarn dev
+
+# build
+yarn build
+```
+
+**yarn 和 npm 区别**
+1. 速度快。yarn install的时候是并行执行的；npm是串行的。
+2. 输出更简洁。
+3. 语义化更清晰。yarn改install为add,uninstall为remove等。
+
+## 系统升级
+
+### 引入 Astro 提高网站性能
+> M1芯片好像不支持，安装失败了，后边再试试吧
+```
+# astro
+yarn add astro
+```
+
+## 相关文档
+
+VuePress2官方文档: https://v2.vuepress.vuejs.org/zh/
